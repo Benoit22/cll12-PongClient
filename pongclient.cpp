0b21e7111f57636f9a24243d063b160a593ab0f6
Benoit22 <benoitrivard22@hotmail.com>
2012-05-03 13:00:36 -0400

    prise 2

--- pongclient.cpp
+++ pongclient.cpp
@@ -11,25 +11,6 @@ PongClient::PongClient(QWidget *parent) :
     ui->setupUi(this);
     m_x = 10;
     m_y = 10;
-   // QRect r1(100, 200, 11, 16);
-    //QPaintEvent eve;
-    //eve.rect(r1);
-    //QRectF rectangle(10.0, 20.0, 80.0, 60.0);
-    //QPainter painter;
-    //painter.setPen(Qt::blue);
-    //painter.setFont(QFont("Arial", 30));
-    //painter.begin(ui->lblJeux);
-    //painter.drawRect(rectangle);
-    //QPainter Palette(this);
-    //Palette.drawRect(10,10,10,10);
-    //Palette.save();
-    //Palette.fillRect(10,10,5,8,Qt::SolidPattern);
-    //ui->label->set;
-
-    //QColor *couleur;
-    //couleur = new QColor(255,255,255);
-    //qDrawPlainRect(&Palette,100,100,10,10,*couleur, 1,0);
-    //ui->lblJeux->
     update();
 }
 void PongClient::paintEvent(QPaintEvent *)
--- pongclient.h
+++ pongclient.h
@@ -2,6 +2,8 @@
 #define PONGCLIENT_H
 
 #include <QMainWindow>
+#include <QWidget>
+    #include <QKeyEvent>
 
 namespace Ui {
 class PongClient;
@@ -14,9 +16,24 @@ class PongClient : public QMainWindow
 public:
     explicit PongClient(QWidget *parent = 0);
     ~PongClient();
-    
+
+protected:
+    void paintEvent(QPaintEvent *);
+    void keyPressEvent(QKeyEvent *);
 private:
     Ui::PongClient *ui;
+    int m_x;
+    int m_y;
+    int m_w;
+    int m_h;
+    int m_x1;
+    int m_y1;
+    int m_w1;
+    int m_h1;
+    int m_x2;
+    int m_y2;
+    int m_w2;
+    int m_h2;
 };
 
 #endif // PONGCLIENT_H
